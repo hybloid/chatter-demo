a58d4c5771cbfec6138e78694d890b6eb042ae1e
Chatter E2E <chatter-e2e@jetbrains.com>
2026-07-12 22:20:20 +0000

    feat: table mutation events

--- pylib/tinydb/__init__.py
+++ pylib/tinydb/__init__.py
@@ -27,6 +27,8 @@ Usage example:
 from .queries import Query, where
 from .storages import Storage, JSONStorage
 from .database import TinyDB
+from .events import subscribe, emit
 from .version import __version__
 
-__all__ = ('TinyDB', 'Storage', 'JSONStorage', 'Query', 'where')
+__all__ = ('TinyDB', 'Storage', 'JSONStorage', 'Query', 'where',
+           'subscribe', 'emit')
